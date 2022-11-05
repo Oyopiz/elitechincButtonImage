189a0cdf16ac4e77e9cc101ac7d1ca1c00ab4545
kevJAMES <kevinochieng023@gmail.com>
2022-11-05 16:55:30 +0300

    first commit

---
+++ buttonImage/src/main/java/com/elitechinc/buttonimage/ButtonImage.java
@@ -0,0 +1,11 @@
+package com.elitechinc.buttonimage;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ButtonImage {
+    public static void s(Context c, String message) {
+
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+    }
+}
